5f8a57a653d159e768491ed5a38b89fcaa6578b8
jonomendelson <jonomendelson@gmail.com>
2016-05-21 09:55:55 -0400

    agaga

--- index.js
+++ index.js
@@ -29,32 +29,43 @@ const BULLET_SPEED = 5;
 var walls = [];
 var tanks = [];
 
-function updateFrame(isUpPressed, isDownPressed, isLeftPressed, isRightPressed) { //Updates the frame, getting the arguments from the sockets
+var stage = "LOBBY";
+
+function updateFrame() { //Updates the frame, getting the arguments from the sockets
 	//Each argument should be an array of booleans, one for each tank
-	for (tankIter = 0; tankIter < tanks.length; tankIter++) {
-		if (isUpPressed[tankIter]) {
-			tanks[tankIter].xPos += (Math.cos(tanks[tankIter].rotation) * TANK_FORWARD_SPEED);
-			tanks[tankIter].yPos += (Math.sin(tanks[tankIter].rotation) * TANK_FORWARD_SPEED);
-		}
-		if (isDownPressed[tankIter]) {
-			tanks[tankIter].xPos -= (Math.cos(tanks[tankIter].rotation) * TANK_BACKWARD_SPEED);
-			tanks[tankIter].yPos -= (Math.sin(tanks[tankIter].rotation) * TANK_BACKWARD_SPEED);
-		}
-		if (isLeftPressed[tankIter]) {
-			tanks[tankIter].rotate(true);
-		}
-		if (isRightPressed[tankIter]) {
-			tanks[tankIter].rotate(false);
+	if(stage == "LOBBY"){ //game hasn't started yet
+		
+	}else if(stage == "GAME"){
+		for (tankIter = 0; tankIter < tanks.length; tankIter++) {
+			if (tanks[tankIter].isUpPressed) {
+				tanks[tankIter].xPos += (Math.cos(tanks[tankIter].rotation) * TANK_FORWARD_SPEED);
+				tanks[tankIter].yPos += (Math.sin(tanks[tankIter].rotation) * TANK_FORWARD_SPEED);
+			}
+			if (isDownPressed[tankIter]) {
+				tanks[tankIter].xPos -= (Math.cos(tanks[tankIter].rotation) * TANK_BACKWARD_SPEED);
+				tanks[tankIter].yPos -= (Math.sin(tanks[tankIter].rotation) * TANK_BACKWARD_SPEED);
+			}
+			if (isLeftPressed[tankIter]) {
+				tanks[tankIter].rotate(true);
+			}
+			if (isRightPressed[tankIter]) {
+				tanks[tankIter].rotate(false);
+			}
 		}
 	}
+	
+	var game_data = [stage, walls, tanks];
+	io.sockets.emit('all_data', game_data);
 }
 
-function makeTank(startX, startY, startRotation) { //Create a tank, and then push it into the tank array
+function makeTank(startX, startY, startRotation, mySocketId) { //Create a tank, and then push it into the tank array
 	var tank = {
 		xPos:startX,
 		yPos:startY,
 		rotation:startRotation,
+		socketid:mySocketId,
 		bullets:[],
+		keypresses:{},
 		rotate:function(rotateLeft) { //rotateLeft is a boolean. If it's true, then the tank will rotate left, otherwise right
 			rotation += (rotateLeft ? TANK_ROTATION_SPEED : -TANK_ROTATION_SPEED)
 		},
@@ -69,5 +80,9 @@ function makeTank(startX, startY, startRotation) { //Create a tank, and then pus
 	}
 	tanks.push(tank);
 }
+
+io.on('connection', function(socket){
+	
+});
 
 window.setInterval(function(){updateFrame();}, FRAMES_PER_SECOND);
