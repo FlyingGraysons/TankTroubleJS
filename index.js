a568563c95c943ff0e5e486cd3a7df3e8bd7293d
ja-San <jake_sanders@horacemann.org>
2016-05-21 08:38:38 -0400

    Fixed constructors, frames, new rotate method

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
+//Server side
+
 const FRAMES_PER_SECOND = 30;
 
 const TANK_LENGTH = 75;
 const TANK_WIDTH = 50;
 
-const TANK_SPEED = 5; //Unit: pixels/second
-const BULLET_SPEED = 5; //Unit: pixels/second
+//Unit: pixels/second
+const TANK_FORWARD_SPEED = 5;
+const TANK_BACKWARD_SPEED = (2/3) * TANK_FORWARD_SPEED;
+const TANK_ROTATION_SPEED = Math.PI * .1;
+const BULLET_SPEED = 5;
 
 var walls = [];
 var tanks = [];
 
-function updateFrame {
-	for (tankIt = 0; tankIt < tanks.length; tankIt++) {
-
+function updateFrame(isUpPressed, isDownPressed, isLeftPressed, isRightPressed) { //Updates the frame, getting the arguments from the sockets
+	//Each argument should be an array of booleans, one for each tank
+	for (tankIter = 0; tankIter < tanks.length; tankIter++) {
+		if (isUpPressed[tankIter]) {
+			tanks[tankIter].xPos += (Math.cos(tanks[tankIter].rotation) * TANK_FORWARD_SPEED);
+			tanks[tankIter].yPos += (Math.sin(tanks[tankIter].rotation) * TANK_FORWARD_SPEED);
+		}
+		if (isDownPressed[tankIter]) {
+			tanks[tankIter].xPos -= (Math.cos(tanks[tankIter].rotation) * TANK_BACKWARD_SPEED);
+			tanks[tankIter].yPos -= (Math.sin(tanks[tankIter].rotation) * TANK_BACKWARD_SPEED);
+		}
+		if (isLeftPressed[tankIter]) {
+			tanks[tankIter].rotate(true);
+		}
+		if (isRightPressed[tankIter]) {
+			tanks[tankIter].rotate(false); 
+		}
 	}
 }
 
-function makeTank (startX, startY, startRotation) {
+function makeTank(startX, startY, startRotation) { //Create a tank, and then push it into the tank array
 	var tank = {
-		var xPos = startX;
-		var yPos = startY;
-		var rotation = startRotation;
-		var bullets = [];
+		xPos:startX,
+		yPos:startY,
+		rotation:startRotation,
+		bullets:[],
+		rotate:function(rotateLeft) { //rotateLeft is a boolean. If it's true, then the tank will rotate left, otherwise right
+			rotation += (rotateLeft ? TANK_ROTATION_SPEED : -TANK_ROTATION_SPEED)
+		},
+		makeBullet:function() {
+			var bullet = {
+				xPos:tank.xPos, // TO DO: Make it the front of the tank, not the middle, where bullets come from
+				yPos:tank.yPos,
+				rotation:tank.rotation,
+			}
+			bullets.push(bullet);
+		},
 	}
 	tanks.push(tank);
-}
-
-function makeBullet (tank) {
-	var bullet = {
-		var xPos = tank.xPos; //Make it the front of the tank, not the middle, where bullets come from
-		var yPos = tank.yPos;
-		var rotation;
-	}
 }
